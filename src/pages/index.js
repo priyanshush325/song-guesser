626ffc1bea8938b1bea24792579480003d4f1409
priyanshush325 <priyanshush325@gmail.com>
2024-09-08 20:06:29 -0700

    updated new date logic to only be handled client side

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,21 +11,22 @@ import Search from "../components/Search";
 import Guesses from "../components/Guesses";
 import {HoverInfo} from "../components/Guesses";
 
-
 export default function Home() {
-
-  const [guesses, setGuesses]  = useState([null, null, null, null, null]);
+  const [guesses, setGuesses] = useState([null, null, null, null, null]);
   const [currentGuess, setCurrentGuess] = useState(0);
   const [artist, setArtist] = useState(null);
   const [genre, setGenre] = useState(null);
   const [album, setAlbum] = useState(null);
   const [showIntro, setShowIntro] = useState(true);
   const [prompt, setPrompt] = useState("-");
+  const [currentDate, setCurrentDate] = useState('');
+
+  useEffect(() => {
+    setCurrentDate(new Date().toLocaleDateString());
+  }, []);
 
   const addGuess = (guess) => {
-    if (currentGuess > 4) {
-        return;
-    }
+    if (currentGuess > 4) return;
     const updatedGuesses = guesses.map((g, index) => 
         index === currentGuess ? guess : g
     );
@@ -33,15 +34,15 @@ export default function Home() {
     setCurrentGuess(currentGuess + 1);
 
     if (guess.correctArtist) {
-        setArtist(guess.artist);
+      setArtist(guess.artist);
     }
 
     if (guess.correctGenre) {
-        setGenre(guess.genre);
+      setGenre(guess.genre);
     }
 
     if (guess.correctAlbum) {
-        setAlbum(guess.album);
+      setAlbum(guess.album);
     }
   };
 
@@ -51,8 +52,6 @@ export default function Home() {
     }
   }, [showIntro]);
 
-  const currentDate = new Date();
-
   const handleGameStart = () => {
     setShowIntro(false);
     setCurrentGuess(0);
@@ -60,44 +59,43 @@ export default function Home() {
     setGenre(null);
     setAlbum(null);
 
-    const date = new Date().toLocaleDateString().split('T')[0]; // Use current date
+    const date = new Date().toLocaleDateString(); // Use current date
     console.log('Current Date:', date);
     
     try {
-        fetch(`/api/prompt?date=${date}`)
+      fetch(`/api/prompt?date=${date}`)
         .then((response) => response.json())
         .then((data) => {
-            setPrompt(data.prompt);
+          setPrompt(data.prompt);
         });
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-}
+  };
 
   return (
-    <div className = "flex flex-col items-center justify-center w-screen min-h-screen text-white p-4 gap-6">
+    <div className="flex flex-col items-center justify-center w-screen min-h-screen text-white p-4 gap-6">
       <dialog id="intro-modal" className="modal text-black">
-          <div className="modal-box flex flex-col justify-center items-center gap-3 overflow-hidden">
-              <h1 className = "font-bold text-3xl">Song Guesser </h1>
-              <p className = "text-lg text-center">Guess a song based on an emoji prompt in 5 guesses</p>
-              <h3 className = "font-bold text-lg hidden sm:block">Scoring System:</h3>
-              <div className = "translate-x-[6.5rem] translate-y-[18rem]">
-                <HoverInfo guessInfo = {{correctArtist: true, correctGenre: true}}/>
-              </div>
-              <form method = "dialog" className = "w-full sm:mt-6 flex justify-center">
-                <button className="btn btn-success text-white w-3/5" onClick = {handleGameStart}>Start <span className = "text-sm italic font-normal"> - {currentDate.toLocaleDateString()}</span></button>
-              </form>
+        <div className="modal-box flex flex-col justify-center items-center gap-3 overflow-hidden">
+          <h1 className="font-bold text-3xl">Song Guesser </h1>
+          <p className="text-lg text-center">Guess a song based on an emoji prompt in 5 guesses</p>
+          <h3 className="font-bold text-lg hidden sm:block">Scoring System:</h3>
+          <div className="translate-x-[6.5rem] translate-y-[18rem]">
+            <HoverInfo guessInfo={{ correctArtist: true, correctGenre: true }} />
           </div>
+          <form method="dialog" className="w-full sm:mt-6 flex justify-center">
+            <button className="btn btn-success text-white w-3/5" onClick={handleGameStart}>Start <span className="text-sm italic font-normal"> - {currentDate}</span></button>
+          </form>
+        </div>
       </dialog>
-      <SongInfo artist = {artist} genre = {genre} album = {album}/>
-      <div className = "w-full md:w-4/5 flex flex-col items-center md:px-20">
-        <Prompt promptText = {prompt}/>
+      <SongInfo artist={artist} genre={genre} album={album} />
+      <div className="w-full md:w-4/5 flex flex-col items-center md:px-20">
+        <Prompt promptText={prompt} />
       </div>
-      <div className = "w-full md:w-4/5 flex flex-col items-center md:px-20">
-        <Search addGuess = {addGuess}/>
+      <div className="w-full md:w-4/5 flex flex-col items-center md:px-20">
+        <Search addGuess={addGuess} />
       </div>
-      <Guesses guesses = {guesses}/>
+      <Guesses guesses={guesses} />
     </div>
   );
 }
-
