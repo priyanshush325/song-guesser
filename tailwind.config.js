b8ec04444f02346450d6913dccf6c66f3935c2e5
priyanshush325 <priyanshush325@gmail.com>
2024-09-08 20:27:06 -0700

    disabled daisyUI dark theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
     },
+  },
+  daisyui: {
+    themes: false,
   },
   plugins: [
     require('daisyui')
